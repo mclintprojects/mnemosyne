b1e96812ad5f4cdcf1ec2d2f48d01d8f816114e7
Clinton Mbah <clintonmbah44@gmail.com>
2024-01-02 10:41:48 +0000

    Add Inter font

--- src/Game.vue
+++ src/Game.vue
@@ -4,17 +4,19 @@
 
 <script>
 export default {
-  name: "App",
+  name: "Game",
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+#game {
+  font-family: "Inter", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100vw;
+  height: 100vh;
+  padding: 30px;
 }
 </style>
